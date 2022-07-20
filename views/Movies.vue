f0df2f88d01e2c5bf4f3a302bc99d73bfa4cc14b
UnityDemas <98840485+UnityDemas@users.noreply.github.com>
2022-07-20 18:52:52 +0200

    Add files via upload

---
+++ views/Movies.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <navBar />
+    <backgroundFilm />
+    <sliderHome />
+    <sliderHomeThree />
+    <sliderHomeFour />
+    <sliderHomeTwo />
+    <footers />
+  </div>
+</template>
+<script>
+import footers from "@/components/footer/footers";
+import backgroundFilm from "@/components/filmler/backgroundFilm";
+import sliderHome from "@/components/Home/sliderHome";
+import sliderHomeTwo from "@/components/Home/sliderHomeTwo";
+import sliderHomeThree from "@/components/Home/sliderHomeThree";
+import sliderHomeFour from "@/components/Home/sliderHomeFour";
+
+export default {
+  components: {
+    footers,
+    backgroundFilm,
+    sliderHome,
+    sliderHomeTwo,
+    sliderHomeThree,
+    sliderHomeFour,
+  },
+};
+</script>
---
+++ views/Series.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <navBar />
+    <backgroundSeries />
+    <diziler />
+    <dayPopularTv />
+    <onerilenDiziler />
+    <onerilenBreaking />
+    <netflix />
+    <footers />
+  </div>
+</template>
+<script>
+import diziler from "@/components/series/diziler";
+import backgroundSeries from "@/components/series/backgroundSeries";
+import dayPopularTv from "@/components/NewPopular/dayPopularTv";
+import onerilenDiziler from "@/components/series/onerilenDiziler";
+import onerilenBreaking from "@/components/series/onerilenBreaking";
+import netflix from "@/components/series/netflix";
+import footers from "@/components/footer/footers.vue";
+
+export default {
+  components: {
+    diziler,
+    backgroundSeries,
+    dayPopularTv,
+    onerilenDiziler,
+    onerilenBreaking,
+    netflix,
+    footers,
+  },
+};
+</script>
